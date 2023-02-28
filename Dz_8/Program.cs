3df6ad01c056a4fcbde6efc10da63b5f966008af
Volodya <skochilovvladimir@yandex.ru>
2023-03-01 02:08:04 +0700

    Dz8

---
+++ Dz_8/Program.cs
@@ -0,0 +1,9 @@
+﻿ //Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i < N)
+{
+    i += 1;
+    if (i % 2 == 0)
+    System.Console.WriteLine(i);
+}
