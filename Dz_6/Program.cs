b18449b171344021212b48d9d8b673ad2c5a068d
Volodya <skochilovvladimir@yandex.ru>
2023-03-01 02:06:05 +0700

    Dz6

---
+++ Dz_6/Program.cs
@@ -0,0 +1,6 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number%2) == 0 )
+    System.Console.WriteLine("Da");
+else
+    System.Console.WriteLine("net");
