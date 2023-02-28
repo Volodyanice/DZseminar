e9a5bd66e38133fd949629aa3ff0ba80cba69bbe
Volodya <skochilovvladimir@yandex.ru>
2023-03-01 02:02:31 +0700

    Dz4

---
+++ Dz_4/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int thrdNumber = Convert.ToInt32(Console.ReadLine());
+int max = firstNumber;
+if (firstNumber>max) max = firstNumber;
+if (secondNumber>max) max = secondNumber;
+if ( thrdNumber > max) max = thrdNumber;
+System.Console.WriteLine(max);
