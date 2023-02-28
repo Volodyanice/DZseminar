9a2cf66a6b6da8fc3c2512ad4716b516852f166f
Volodya <skochilovvladimir@yandex.ru>
2023-03-01 01:55:09 +0700

    Dz_2

---
+++ Dz_2/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber>secondNumber)
+    Console.WriteLine($"max = {firstNumber}");
+else
+    Console.WriteLine($"max = {secondNumber}");
